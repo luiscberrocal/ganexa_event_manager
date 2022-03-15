7c1541369a996398e2d620b70c3205bdc2c86513
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-03-15 05:15:53 -0500

    Fixed issue with username.

--- ganexa_event_manager/users/adapters.py
+++ ganexa_event_manager/users/adapters.py
@@ -14,11 +14,11 @@ class AccountAdapter(DefaultAccountAdapter):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
     def save_user(self, request, user, form, commit=True):
-        user = super(AccountAdapter, self).save_user(request, user, form, commit=commit)
-        user.last_name = 'Kent'
-        user.first_name = 'Clark'
+        super(AccountAdapter, self).save_user(request, user, form, commit=commit)
+        user.last_name = form.cleaned_data['last_name']
+        user.first_name = form.cleaned_data['first_name']
         user.save()
-        logger.debug(f'AccountAdapter.save_user {user.last_name} {user.first_name} {user.username}')
+        logger.debug(f'>>>> AccountAdapter.save_user {user.last_name} {user.first_name} {user.username}')
         return user
 
 
