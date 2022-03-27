d80dd5259fc3e2690d5e2a6c00a0f96078b9cadd
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-03-27 10:44:58 -0500

    Delete hit functionality

--- ganexa_event_manager/golf/urls.py
+++ ganexa_event_manager/golf/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import range_hits_view, save_hit_view, list_player_hits_view
+from .views import range_hits_view, save_hit_view, list_player_hits_view, delete_range_hit
 
 app_name = "golf"
 urlpatterns = [
     path("range-hits/", view=range_hits_view, name="range-hits"),
     path("save-hit/", view=save_hit_view, name="save-hit"),
     path("player-hits/", view=list_player_hits_view, name="player-hits"),
+    path("player-hit/delete/<int:pk>/", view=delete_range_hit, name="player-hit-delete"),
 ]
