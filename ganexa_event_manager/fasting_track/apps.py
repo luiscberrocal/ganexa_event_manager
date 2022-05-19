41b6c82f05bdc0a5e6d506478c4527af0edf0ed9
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-05-18 19:51:24 -0500

    fasting

---
+++ ganexa_event_manager/fasting_track/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FastingTrackConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fasting_track'
---
+++ ganexa_event_manager/fasting_track/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class FastingSession(AuditableModel, TimeStampedModel)
