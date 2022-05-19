5a1abaf3355e229fde735d47211f74aca74f9c40
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-05-18 20:17:08 -0500

    Starting model.

--- ganexa_event_manager/fasting_track/models.py
+++ ganexa_event_manager/fasting_track/models.py
@@ -1,7 +1,32 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
 from model_utils.models import TimeStampedModel
+from django.utils.translation import ugettext_lazy as _
+from ganexa_event_manager.core.models import AuditableModel
 
 
-class FastingSession(AuditableModel, TimeStampedModel)
+class FastingSession(AuditableModel, TimeStampedModel):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'), related_name='fasting_sessions',
+                             on_delete=models.PROTECT)
+    start_date = models.DateTimeField(_('Start date'))
+    end_date = models.DateTimeField(_('End date'), null=True, blank=True)
+    comments = models.CharField(_('Comments'), max_length=180, null=True, blank=True)
+
+    @property
+    def duration(self):
+        if self.end_date is None:
+            end_date = timezone.now()
+        else:
+            end_date = self.end_date
+        return end_date - self.start_date
+
+    class Meta:
+        ordering = ('start_date',)
+
+    def __str__(self):
+        return f'{self.user.username} {self.start_date} {self.duration}'
+
