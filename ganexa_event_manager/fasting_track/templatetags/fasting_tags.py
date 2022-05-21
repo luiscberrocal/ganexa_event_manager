2d9b3caf6db3ba3e541d126c26738a1b94dfc113
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-05-21 16:22:43 -0500

    Basic fasting.

---
+++ ganexa_event_manager/fasting_track/templatetags/fasting_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+
+def percentage(value):
+    return '{0:.2%}'.format(value)
+
+
+def hours(value):
+    hours_text = _('hours')
+    hours_template = '{0:,.2f}'
+    hours_value = hours_template.format(value)
+    return f'{hours_value} {hours_text}'
+
+
+register.filter('percentage', percentage)
+register.filter('hours', hours)
